443fc85803cb082cd16c9f65d154f80c0b26a5fe
Kjersti Berg <kjersti.berg@linkmobility.com>
2016-03-30 22:03:18 +0200

    Count ones

---
+++ Yatzhee/YatzheeScorer.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Yatzhee
+{
+    public class YatzheeScorer
+    {
+        public int Score(string roll, Category category)
+        {
+            return roll.Count(c => c == '1');
+        }
+    }
+}
---
+++ Yatzhee/YatzheeScorerTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Yatzhee
+{
+    [TestFixture]
+    public class YatzheeScorerTest
+    {
+        [TestCase("1,2,3,4,5", 1)]
+        [TestCase("2,2,3,4,5", 0)]
+        public void ScoreOnes(string roll, int expectedScore)
+        {
+            var score = new YatzheeScorer().Score(roll, Category.Ones);
+            score.Should().Be(expectedScore);
+        }
+    }
+
+    public enum Category
+    {
+        Ones
+    }
+}
