4870943bd489b4be2f8707e05db3da336717cf94
Kjersti Berg <kjersti.berg@linkmobility.com>
2016-03-30 22:59:12 +0200

    Added one pair

--- Yatzhee/Category.cs
+++ Yatzhee/Category.cs
@@ -1,21 +1,31 @@
-using System;
 using System.Linq;
 
 namespace Yatzhee
 {
-    public class Category
+    public abstract class Category
+    {
+        public static Category Ones = new Sum(1);
+        public static Category Twos = new Sum(2);
+        public static Category Threes = new Sum(3);
+        public static Category Fours = new Sum(4);
+        public static Category Fives = new Sum(5);
+        public static Category Sixes = new Sum(6);
+
+        public static Category OnePair = new Count(2);
+
+        public abstract int Score(string roll);
+    }
+
+    internal class Sum : Category
     {
         private readonly int _match;
 
-        public static Category Ones = new Category(1); 
-        public static Category Twos = new Category(2);
-
-        private Category(int match)
+        protected internal Sum(int match)
         {
             _match = match;
         }
 
-        public int Score(string roll)
+        public override int Score(string roll)
         {
             return roll.Count(ch => ch == AsciiValue(_match)) * _match;
         }
@@ -23,6 +33,31 @@ namespace Yatzhee
         private static int AsciiValue(int v)
         {
             return v + '0';
+        }
+    }
+
+    internal class Count : Category
+    {
+        private readonly int _count;
+
+        protected internal Count(int count)
+        {
+            _count = count;
+        }
+
+        public override int Score(string roll)
+        {
+            var highest = 
+                roll.Split(',')
+                .GroupBy(c => c)
+                .Where(grp => grp.Count() >= _count)
+                .OrderByDescending(grp => grp.Key)
+                .FirstOrDefault();
+
+            if (highest == null)
+                return 0;
+
+            return int.Parse(highest.Key) * _count;
         }
     }
 }
--- Yatzhee/YatzheeScorerTest.cs
+++ Yatzhee/YatzheeScorerTest.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Yatzhee
@@ -21,6 +22,42 @@ namespace Yatzhee
         public void ScoreTwos(string roll, int expectedScore)
         {
             var score = new YatzheeScorer().Score(roll, Category.Twos);
+            score.Should().Be(expectedScore);
+        }
+
+        [TestCase("1,2,3,4,5", 3)]
+        [TestCase("2,3,3,3,5", 9)]
+        [TestCase("1,1,1,4,5", 0)]
+        public void ScoreThrees(string roll, int expectedScore)
+        {
+            var score = new YatzheeScorer().Score(roll, Category.Threes);
+            score.Should().Be(expectedScore);
+        }
+
+        [TestCase("1,2,3,4,5", 4)]
+        [TestCase("2,3,3,3,5", 0)]
+        [TestCase("1,4,4,4,4", 16)]
+        public void ScoreFours(string roll, int expectedScore)
+        {
+            var score = new YatzheeScorer().Score(roll, Category.Fours);
+            score.Should().Be(expectedScore);
+        }
+
+        [TestCase("1,2,3,4,5", 5)]
+        [TestCase("2,3,3,3,1", 0)]
+        [TestCase("5,5,4,5,4", 15)]
+        public void ScoreFives(string roll, int expectedScore)
+        {
+            var score = new YatzheeScorer().Score(roll, Category.Fives);
+            score.Should().Be(expectedScore);
+        }
+
+        [TestCase("1,2,3,4,5", 0)]
+        [TestCase("2,3,3,3,1", 6)]
+        [TestCase("5,5,4,5,4", 10)]
+        public void ScoreOnePair(string roll, int expectedScore)
+        {
+            var score = new YatzheeScorer().Score(roll, Category.OnePair);
             score.Should().Be(expectedScore);
         }
     }
